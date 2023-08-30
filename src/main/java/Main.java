7bb55a3d48c159ec4dd8d1de09791af75c811fb3
LuzRdgz <luzrdgz15@gmail.com>
2023-08-30 01:04:24 -0700

    Ejercicio I, II, III, IV, V y VI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,39 @@
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-       /* String cadena1 = "Hola Mundo";
+        String cadena1 = "Hola Mundo";
         String cadena2 = "HOla muNdO";
         String cadena3 = "Go";
         String cadena4 = "a215632511@uson.mx";
-        String cadena5 = "ISI2021-1.txt"; */
+        String cadena5 = "ISI2021-1.txt";
 
 
         ExprReg er = new ExprReg();
 
         System.out.print("1: ");
-        er.fraseExacta((args[0]));
-        /*System.out.print("2: ");
+        er.fraseExacta(cadena1);
+        System.out.print("2: ");
         er.fraseMayusMin(cadena2);
         System.out.print("3: ");
         er.buscarPalabra(cadena3);
         System.out.print("4: ");
         er.validarCorreo(cadena4);
         System.out.print("5: ");
-        er.validarNombreArchivo(cadena5);*/
+        er.validarNombreArchivo(cadena5);
+
+        System.out.println("==================================");
+
+        // Con argumentos
+         //Los que tienen espacios no los pude poner desde Modify Run Configuration
+
+        System.out.print("3: ");
+        er.buscarPalabra(args[0]);
+        System.out.print("4: ");
+        er.validarCorreo(args[1]);
+        System.out.print("5: ");
+        er.validarNombreArchivo(args[2]);
 
     }
 
