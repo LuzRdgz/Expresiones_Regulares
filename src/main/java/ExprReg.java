13626fe82c12ee693884a404f50bad93c7e0b14c
LuzRdgz <luzrdgz15@gmail.com>
2023-08-30 00:09:38 -0700

    Ejercicio I, II, III, IV y V

--- src/main/java/ExprReg.java
+++ src/main/java/ExprReg.java
@@ -3,10 +3,10 @@ import java.util.regex.Pattern;
 
 public class ExprReg {
 
-    public void fraseExacta(String cadena){
+    public void fraseExacta(String cadena1){
 
-        Pattern pt = Pattern.compile("[hH][oO][lL][aA]\s[mM][uU][nN][dD][oO]");
-        Matcher mt = pt.matcher(cadena);
+        Pattern pt = Pattern.compile("Hola Mundo");
+        Matcher mt = pt.matcher(cadena1);
 
         if (mt.matches()){
             System.out.println("EXPRESION VALIDA :)");
@@ -15,6 +15,58 @@ public class ExprReg {
         }
 
     }
+    public void fraseMayusMin(String cadena2){
+
+        Pattern pt = Pattern.compile("[hH][oO][lL][aA]\s[mM][uU][nN][dD][oO]");
+        Matcher mt = pt.matcher(cadena2);
+
+        if (mt.matches()){
+            System.out.println("EXPRESION VALIDA :)");
+        }else{
+            System.out.println("EXPRESION NO VALIDA :(");
+        }
+
+    }
+
+    public void buscarPalabra(String cadena3){
+
+        Pattern pt = Pattern.compile("Java|Python|Go|Pascal|Perl");
+        Matcher mt = pt.matcher(cadena3);
+
+        if (mt.matches()){
+            System.out.println("EXPRESION VALIDA :)");
+        }else{
+            System.out.println("EXPRESION NO VALIDA :(");
+        }
+
+    }
+    public void validarCorreo(String cadena4){
+
+        Pattern pt = Pattern.compile(".*@unison[.]mx|.*@uson[.]mx");
+        Matcher mt = pt.matcher(cadena4);
+
+        if (mt.matches()){
+            System.out.println("EXPRESION VALIDA :)");
+        }else{
+            System.out.println("EXPRESION NO VALIDA :(");
+        }
+
+    }
+
+    public void validarNombreArchivo(String cadena5){
+
+        Pattern pt = Pattern.compile("^ISI[0-9]{4}-[12][.]");
+        Matcher mt = pt.matcher(cadena5);
+
+        if (mt.matches()){
+            System.out.println("EXPRESION VALIDA :)");
+        }else{
+            System.out.println("EXPRESION NO VALIDA :(");
+        }
+
+    }
+
+
 
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String cadena = "hoLa MuNdo";
+        String cadena1 = "Hola Mundo";
+        String cadena2 = "HOla muNdO";
+        String cadena3 = "Go";
+        String cadena4 = "a215632511@uson.mx";
+        String cadena5 = "ISI2021-1.";
+        String cadena6 = "ISI2021-3.txt";
+
 
         ExprReg er = new ExprReg();
 
-        er.fraseExacta(cadena);
+        System.out.print("1: ");
+        er.fraseExacta(cadena1);
+        System.out.print("2: ");
+        er.fraseMayusMin(cadena2);
+        System.out.print("3: ");
+        er.buscarPalabra(cadena3);
+        System.out.print("4: ");
+        er.validarCorreo(cadena4);
+        System.out.print("5: ");
+        er.validarNombreArchivo(cadena5);
+        er.validarNombreArchivo(cadena6);
 
     }
 
