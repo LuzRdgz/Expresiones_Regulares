f48dd7b1f53760dd762f195ab56b3b88a8870926
LuzRdgz <luzrdgz15@gmail.com>
2023-08-29 23:08:08 -0700

    Ejercicio I

--- src/main/java/ExprReg.java
+++ src/main/java/ExprReg.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ExprReg {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExprReg {
+
+    public void fraseExacta(String cadena){
+
+        Pattern pt = Pattern.compile("[hH][oO][lL][aA]\s[mM][uU][nN][dD][oO]");
+        Matcher mt = pt.matcher(cadena);
+
+        if (mt.matches()){
+            System.out.println("EXPRESION VALIDA :)");
+        }else{
+            System.out.println("EXPRESION NO VALIDA :(");
+        }
+
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        String cadena = "hoLa MuNdo";
+
+        ExprReg er = new ExprReg();
+
+        er.fraseExacta(cadena);
+
+    }
+
 }
